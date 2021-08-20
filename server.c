dd12eb24b4b64ea3be2a77cb0429c411f4c8c433
ojacomarket <elfutbolesmivida@mail.ru>
2021-08-20 18:54:17 +0300

    add libraries

---
+++ server.c
@@ -0,0 +1,14 @@
+//All necessary libraries to work with sockets (BE SURE YOU HAVE "gcc" compiler
+//pre-installed on your PC
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 1;
+}
